b8196adf01f45f920741dc10416eecb3f857514a
Subhasish <subhasishgarnayak2@gmail.com>
2025-04-11 23:27:18 +0530

    fixed everything

---
+++ src/components/CreativeSquares.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreativeSquares: React.FC = () => {
+    return (
+        <div>
+            <h1>Creative Squares Component</h1>
+        </div>
+    );
+};
+
+export default CreativeSquares;
